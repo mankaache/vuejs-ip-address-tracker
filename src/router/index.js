4a5f97d3b728afe5a652eb395f0b28604e895223
mankaa <mankaache10@gmail.com>
2023-01-18 09:19:38 +0100

    adding tailwindcss and leaflet to the project

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta:{
+      title:"IP Address Tracker"
+    }
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to,from,next) => {
+  document.title = `${to.meta.title}`
+  next()
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- search / results -->
+    <div class="back flex justify-center relative px-4 pt-8 pb-32 "></div>
+  </div>
+  </template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.back{
+background: radial-gradient(65% 100% at 50% 0%, #00FF94 0%, rgba(0, 255, 148, 0.25) 100%), linear-gradient(230deg, #000000 25%, #170059 100%), linear-gradient(215deg, #FFEBB9 10%, #19004E 80%), radial-gradient(100% 245% at 100% 100%, #FFFFFF 0%, #000353 100%), linear-gradient(125deg, #1400FF 0%, #3A0000 100%), linear-gradient(225deg, #00F0FF 30%, #000B6F 45%, #00EBFC 45%, #001676 65%, #00E1F6 65%, #001676 85%, #00ECFD 85%, #001676 100%), linear-gradient(135deg, #00F0FF 0%, #000B6F 15%, #00EBFC 15%, #001676 35%, #00E1F6 35%, #001676 55%, #00ECFD 55%, #001676 100%);
+background-blend-mode: soft-light, screen, overlay, overlay, difference, overlay, normal;
+}
+</style>
