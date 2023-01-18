fb1a14e78face931587133084d626708d48dd133
mankaa <mankaache10@gmail.com>
2023-01-18 09:55:08 +0100

    added the top part or header of the file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,70 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- search / results -->
-    <div class="back flex justify-center relative px-4 pt-8 pb-32 "></div>
+    <div class="back flex justify-center relative px-4 pt-8 pb-32">
+      <!-- search input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-center text-white text-4xl font-bold pb-8">
+          IP Adress Tracker
+        </h1>
+        <div class="flex">
+          <input
+            type="text"
+            placeholder="Search for any IP address or leave it empty to your IP info"
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+          />
+          <i
+            class="fas fa-chevron-right cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center"
+          ></i>
+        </div>
+      </div>
+    </div>
   </div>
-  </template>
+</template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'HomeView',
-  components: {
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
 
 <style scoped>
-.back{
-background: radial-gradient(65% 100% at 50% 0%, #00FF94 0%, rgba(0, 255, 148, 0.25) 100%), linear-gradient(230deg, #000000 25%, #170059 100%), linear-gradient(215deg, #FFEBB9 10%, #19004E 80%), radial-gradient(100% 245% at 100% 100%, #FFFFFF 0%, #000353 100%), linear-gradient(125deg, #1400FF 0%, #3A0000 100%), linear-gradient(225deg, #00F0FF 30%, #000B6F 45%, #00EBFC 45%, #001676 65%, #00E1F6 65%, #001676 85%, #00ECFD 85%, #001676 100%), linear-gradient(135deg, #00F0FF 0%, #000B6F 15%, #00EBFC 15%, #001676 35%, #00E1F6 35%, #001676 55%, #00ECFD 55%, #001676 100%);
-background-blend-mode: soft-light, screen, overlay, overlay, difference, overlay, normal;
+.back {
+  background: radial-gradient(
+      65% 100% at 50% 0%,
+      #00ff94 0%,
+      rgba(0, 255, 148, 0.25) 100%
+    ),
+    linear-gradient(230deg, #000000 25%, #170059 100%),
+    linear-gradient(215deg, #ffebb9 10%, #19004e 80%),
+    radial-gradient(100% 245% at 100% 100%, #ffffff 0%, #000353 100%),
+    linear-gradient(125deg, #1400ff 0%, #3a0000 100%),
+    linear-gradient(
+      225deg,
+      #00f0ff 30%,
+      #000b6f 45%,
+      #00ebfc 45%,
+      #001676 65%,
+      #00e1f6 65%,
+      #001676 85%,
+      #00ecfd 85%,
+      #001676 100%
+    ),
+    linear-gradient(
+      135deg,
+      #00f0ff 0%,
+      #000b6f 15%,
+      #00ebfc 15%,
+      #001676 35%,
+      #00e1f6 35%,
+      #001676 55%,
+      #00ecfd 55%,
+      #001676 100%
+    );
+  background-blend-mode: soft-light, screen, overlay, overlay, difference,
+    overlay, normal;
 }
 </style>
